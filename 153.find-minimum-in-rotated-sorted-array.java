b669434b6a9d357c0b45b2056f2ef323e3980355
YenChen Liu <yenchen.liu@ubiik.com>
2021-05-15 13:57:09 +0800

    add 153

---
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=153 lang=java
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int findMin(int[] nums) {
+    
+        int l=0;
+        int r=nums.length-1;
+        int mid=0;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(mid>0&&nums[mid]<nums[mid-1])
+                break;
+
+            if(nums[mid]<nums[r])
+                r=mid-1;
+            else
+                l=mid+1;
+
+        }
+
+        return nums[mid];
+
+    }
+}
+// @lc code=end
+
