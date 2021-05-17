19e40b72fb738207fe565dff09c4fa0c6ed231b2
Yen-Chen Liu <kenliu@cadence.com>
2021-05-17 15:17:52 +0800

    add 140

--- 140.word-break-ii.java
+++ 140.word-break-ii.java
@@ -6,8 +6,26 @@
 
 // @lc code=start
 class Solution {
+    List<String> res =new ArrayList<String>();
     public List<String> wordBreak(String s, List<String> wordDict) {
-        
+        dfs(s,wordDict,"");
+        return res;
+    }
+    public void dfs(String s,List<String> wordDict, String cur){
+        if(0==s.length()){
+            res.add(cur);
+            return;
+        }
+        for(String word:wordDict){
+            if(s.startsWith(word)){
+                String tmp="";
+                if(cur.length()==0)
+                    tmp=word;
+                else
+                    tmp=cur+ " " + word;
+                dfs(s.substring(word.length()),wordDict,tmp);
+            }
+        }
     }
 }
 // @lc code=end
