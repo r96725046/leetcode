ca1589c958e4ebd53a5efac167e414fd38cc0815
YenChen Liu <yenchen.liu@ubiik.com>
2021-05-24 20:29:58 +0800

    add comments for 33

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -7,30 +7,33 @@
 // @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
+        
         if(nums.length==0)
             return -1;
         int l=0;
         int r=nums.length-1;
-        //four cases
+        int mid=0;
         while(l<=r){
-
-            int mid=(l+r)/2;
+            mid=l+(r-l)/2;
 
             if(nums[mid]==target)
-                return mid;    
-            
-            if(nums[mid]<nums[r]){
-                if(target>nums[mid]&&target<=nums[r])
-                    l=mid+1;
-                else
-                    r=mid-1;
-            }else{
-                if(target<nums[mid]&&target>=nums[l])
-                    r=mid-1;
-                else
-                    l=mid+1;
-            }
+                return mid;
 
+            if(nums[mid]<nums[r]){
+                //check increasing seq only
+                if(target>nums[mid]&&target<=nums[r]){
+                    l=mid+1;
+                }else{
+                    r=mid-1;
+                }
+            }else{
+                 //check increasing seq only
+                if(target<nums[mid]&&target>=nums[l]){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
+                }
+            }
         }
 
         return -1;
