2d0164955dbadd8d415f75e017351d7de10acc57
YenChen Liu <yenchen.liu@ubiik.com>
2021-05-25 21:51:49 +0800

    add 31

---
+++ 31.next-permutation.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=31 lang=java
+ *
+ * [31] Next Permutation
+ */
+
+// @lc code=start
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+        
+        int i;
+        //first i<i+1
+        for(i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1])
+                break;
+        }
+        int j;
+        if(i>=0){
+            //first j>i from end of list
+            for(j=nums.length-1;j>=i;j--)
+            {
+                if(nums[j]>nums[i]){
+                    swap(nums,i,j);
+                    break;
+                }
+            }
+            
+        }    
+        reverse(nums,i<0?0:i+1);
+
+    }
+    public void swap(int[] nums,int i,int j){
+        int tmp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=tmp;
+
+    }
+    public void reverse(int[] nums,int i){
+        int r=nums.length-1;
+        while(i<r)
+            swap(nums,i++,r--);
+    }
+}
+// @lc code=end
+
