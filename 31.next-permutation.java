cbb025f0d1b7ab857787d717854c12994139105b
YenChen Liu <yenchen.liu@ubiik.com>
2021-05-26 20:51:07 +0800

    revise 31

--- 31.next-permutation.java
+++ 31.next-permutation.java
@@ -18,28 +18,24 @@ class Solution {
         int j;
         if(i>=0){
             //first j>i from end of list
-            for(j=nums.length-1;j>=i;j--)
+            for(j=nums.length-1;j>i;j--)
             {
                 if(nums[j]>nums[i]){
                     swap(nums,i,j);
                     break;
                 }
             }
-            
         }    
-        reverse(nums,i<0?0:i+1);
-
+        reverse(nums,i<0?0:i+1,nums.length-1);
     }
     public void swap(int[] nums,int i,int j){
         int tmp=nums[i];
         nums[i]=nums[j];
         nums[j]=tmp;
-
     }
-    public void reverse(int[] nums,int i){
-        int r=nums.length-1;
-        while(i<r)
-            swap(nums,i++,r--);
+    public void reverse(int[] nums,int i,int j){
+        while(i<j)
+            swap(nums,i++,j--);
     }
 }
 // @lc code=end
