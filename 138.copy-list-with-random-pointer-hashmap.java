eda4673504331c1cb06b84400ef878e17ee8f35c
YenChen Liu <yenchen.liu@ubiik.com>
2021-06-08 23:30:56 +0800

    add 138

---
+++ 138.copy-list-with-random-pointer-hashmap.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+
+        HashMap<Node,Node> map=new HashMap<>();
+        Node runner=head;
+        while(runner!=null){
+            map.put(runner,new Node(runner.val));
+            runner=runner.next;
+        }
+
+        runner=head;
+        while(runner!=null){
+            Node cp=map.get(runner);
+            cp.next=map.get(runner.next);
+            cp.random=map.get(runner.random);
+            runner=runner.next;
+        }
+        return map.get(head);
+    }
+}
+// @lc code=end
+
---
+++ 138.copy-list-with-random-pointer.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=138 lang=java
+ *
+ * [138] Copy List with Random Pointer
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+
+        Node runner=head;
+
+        while(runner!=null){
+            Node next=runner.next;
+            runner.next=new Node(runner.val);
+            runner.next.next=next;
+            runner=runner.next.next;
+        }
+        runner=head;
+        while(runner!=null){
+            if(runner.random!=null){
+               Node random=runner.random;
+                runner.next.random=random.next;// *** random.next
+            }
+            runner=runner.next.next;
+        }
+        runner=head;
+        Node cphead=new Node(0);
+        Node cprunner=cphead;
+        while(runner!=null){
+            cprunner.next=runner.next;
+            runner.next=runner.next.next;
+            
+            runner=runner.next;
+            cprunner=cprunner.next;
+        }
+        cprunner.next=null;
+        return cphead.next;
+    }
+}
+// @lc code=end
+
