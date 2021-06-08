399824b75730e8d21fec0502028ccb973220888f
Yen-Chen Liu <kenliu@cadence.com>
2021-06-08 12:11:25 +0800

    add 297 dfs bfs

---
+++ 297.serialize-and-deserialize-binary-tree-bfs.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=297 lang=java
+ *
+ * [297] Serialize and Deserialize Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+public class Codec {
+    // ***
+    // bfs
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root==null)return "";
+        StringBuilder sb =new StringBuilder();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode node=q.poll();
+            if(node==null){
+                sb.append("#").append(",");
+                continue;
+            }
+            sb.append(node.val+",");
+            q.offer(node.left);
+            q.offer(node.right);
+
+        }
+        
+        return sb.toString();
+
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data=="")return null;
+        Queue<TreeNode> q=new LinkedList<>();
+        String[] arr=data.split(",");
+        TreeNode head=new TreeNode(Integer.valueOf(arr[0]));
+        q.offer(head);
+   
+        for(int i=1;i<arr.length;i++){
+            TreeNode parent=q.poll();
+            if(!arr[i].equals("#")){
+                parent.left=new TreeNode(Integer.valueOf(arr[i]));
+                q.offer(parent.left);
+            }
+            i++;
+            if(!arr[i].equals("#")){
+                parent.right=new TreeNode(Integer.valueOf(arr[i]));
+                q.offer(parent.right);
+            }
+        }
+
+        return head;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+// @lc code=end
+
---
+++ 297.serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=297 lang=java
+ *
+ * [297] Serialize and Deserialize Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+public class Codec {
+    // ***
+    // dfs
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        
+        StringBuilder sb=new StringBuilder();
+        build(root,sb);
+        return sb.toString();
+
+    }
+    private void build(TreeNode root, StringBuilder sb){
+
+        if(root==null){
+            sb.append("#").append(",");
+            return;
+        }
+        sb.append(root.val).append(",");
+        build(root.left,sb);
+        build(root.right,sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> q=new LinkedList<>();
+        String[] arr=data.split(",");
+        for(String s:arr)
+            q.offer(s);
+        return buildTree(q);
+    }
+    private TreeNode buildTree(Queue<String> q){
+        String s=q.poll();
+        if(s.equals("#"))
+            return null;
+
+        TreeNode node=new TreeNode(Integer.valueOf(s));
+        node.left=buildTree(q);
+        node.right=buildTree(q);
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+// @lc code=end
+
