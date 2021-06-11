1018942d9dfe873145830bd1105596b4d2a4b7f8
YenChen Liu <yenchen.liu@ubiik.com>
2021-06-11 22:24:18 +0800

    add commnets

--- 73.set-matrix-zeroes.java
+++ 73.set-matrix-zeroes.java
@@ -7,37 +7,50 @@
 // @lc code=start
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // first row and first column
-        boolean r=false;
-        boolean c=false;
+        // **
+        // first row and first column as index
+        // 1. fr fc
+        // 2. i,j start from 1. set index to 0
+        // 3. i,j start from 1. if index = 0, set ij=0
+        // 4. if fr fc, set first row/col to 0
+        boolean fr=false;
+        boolean fc=false;
+
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    if(i==0)
-                        r=true;
-                    if(j==0)
-                        c=true;                    
+            if(matrix[i][0]==0)
+                fc=true;
+        }
+        for(int j=0;j<matrix[0].length;j++)
+        {
+            if(matrix[0][j]==0)
+                fr=true;
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]==0)
+                {
                     matrix[i][0]=0;
                     matrix[0][j]=0;
                 }
             }
         }
         for(int i=1;i<matrix.length;i++){
-            for(int j=1;j<matrix[0].length;j++){
+            for(int j=1;j<matrix[0].length;j++)
+            {
                 if(matrix[0][j]==0||matrix[i][0]==0)
                     matrix[i][j]=0;
             }
         }
-       
-        if(r){
-            for(int j=0;j<matrix[0].length;j++)
-                matrix[0][j]=0;
-        }
-        if(c){
+        if(fc){
             for(int i=0;i<matrix.length;i++)
                 matrix[i][0]=0;
         }
-    }
+        if(fr){
+            for(int j=0;j<matrix[0].length;j++)
+                matrix[0][j]=0;
+        }    
+    }    
 }
 // @lc code=end
 
