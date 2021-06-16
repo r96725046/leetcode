35a0a54394948f84deb191afac96990ab4ee821a
YenChen Liu <yenchen.liu@ubiik.com>
2021-06-16 21:31:09 +0800

    revise 797

--- 797.all-paths-from-source-to-target.java
+++ 797.all-paths-from-source-to-target.java
@@ -6,21 +6,28 @@
 import java.util.*;
 // @lc code=start
 class Solution {
+    // **
+    // 1.path.add(0)
     List<List<Integer>> list=new ArrayList<List<Integer>>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        dfs(graph,0,new ArrayList<Integer>());
+        if(graph.length==0)return list;
+        ArrayList<Integer> path=new ArrayList<Integer>();
+        path.add(0);
+        dfs(graph,0,path);
         return list;
     }
-    public void dfs(int[][] graph,int index,ArrayList<Integer> cur){
-        cur.add(index);
-        if(index==graph.length-1)
-            list.add(new ArrayList<Integer>(cur));
-        int[] nodes=graph[index];
-
-        for(int node:nodes){
-            dfs(graph,node,cur);
+    private void dfs(int[][] graph,int index,ArrayList<Integer> res){
+        if(index==graph.length-1){
+            list.add(new ArrayList<Integer>(res));
+            return;
         }
-        cur.remove(cur.size()-1);
+        int[] target=graph[index];
+        for(int node:target){
+            res.add(node);
+            dfs(graph,node,res);
+            res.remove(res.size()-1);
+        }
+
     }
 }
 // @lc code=end
