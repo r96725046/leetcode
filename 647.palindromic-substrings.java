451424c138bd730d2618cf2592d7bd0dd1351fa2
YenChen Liu <yenchen.liu@ubiik.com>
2021-06-16 21:42:57 +0800

    add commnets

--- 647.palindromic-substrings.java
+++ 647.palindromic-substrings.java
@@ -6,9 +6,12 @@
 
 // @lc code=start
 class Solution {
+    // **
+    // 1.i<s.length() && i+1 is ok
+    // 2.while(i>=0&&j<s.length()&&s.charAt(i)==s.charAt(j))
+    // 1
     int res=0;
     public int countSubstrings(String s) {
-        
         for(int i=0;i<s.length();i++){
             check(s,i,i);
             check(s,i,i+1);
