7c7245d0bee3430b02eec0f87cb52f5af0c38f49
Yen-Chen Liu <kenliu@cadence.com>
2021-06-17 17:03:19 +0800

    revise 84

--- 84.largest-rectangle-in-histogram.java
+++ 84.largest-rectangle-in-histogram.java
@@ -9,35 +9,40 @@ class Solution {
     // **
     // left && right arr =>max and min
     // 1.for+while
-    // 2.i++ p--, i-- p++
+    // 2.j=i-1 i++ j--,j=i+1 i-- j++
     public int largestRectangleArea(int[] heights) {
 
-        int[] left=new int[heights.length];
-        int[] right=new int[heights.length];
-        right[right.length-1]=right.length;
-        left[0]=-1;
-        //Scan left items
-        for(int i=1;i<heights.length;i++){
-            int p=i-1;
-            while(p>=0&&heights[p]>=heights[i]){
-                p=left[p];//use left[p] instead of p--
-            }
-            left[i]=p;
-        }
-        //Scan right items
-        for(int i=right.length-2;i>=0;i--){
-            int p=i+1;
-            while(p<=right.length-1&&heights[p]>=heights[i])
-                p=right[p];
-            right[i]=p;
-        }
+       int[] left=new int[heights.length];
+       int[] right=new int[heights.length];
 
-        int max=0;
+       left[0]=-1;
+       for(int i=1;i<heights.length;i++){
+           int j=i-1;
+           while(j>=0){
+               if(heights[j]>=heights[i])
+                   j=left[j];
+               else
+                    break;
+           }
+           left[i]=j;
+       }
+       right[right.length-1]=right.length;
+       for(int i=heights.length-1;i>=0;i--){
+           int j=i+1;
+           while(j<heights.length){
+               if(heights[j]>=heights[i])
+                   j=right[j];
+               else
+                   break;
+           }
+           right[i]=j;
+       }
 
-        for(int i=0;i<heights.length;i++)
-            max=Math.max(max,heights[i]*(right[i]-left[i]-1));
-        return max;
-        
+       int max=0;
+       for(int i=0;i<heights.length;i++){
+           max=Math.max(max,heights[i]*(right[i]-left[i]-1));
+       }
+       return max;
     }
 }
 // @lc code=end
