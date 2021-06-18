4fdfb3532006a22a068052dce611dc95a4841982
YenChen Liu <yenchen.liu@ubiik.com>
2021-06-18 23:53:27 +0800

    add prime number

--- 140.word-break-ii.java
+++ 140.word-break-ii.java
@@ -7,32 +7,31 @@
 // @lc code=start
 class Solution {
     // ***
-    // 1.startsWith
+    // 1.String.start"s"With()
     // 2.tmp string
     // 3.cache***
+    // 4.s.length()==0 list.add("")
     // 1
     HashMap<String,List<String>> map=new HashMap<>();
     public List<String> wordBreak(String s, List<String> wordDict) {
-        return dfs(s,wordDict);
+       return dfs(s,wordDict);
     }
-    public List<String> dfs(String s,List<String> wordDict){
-        if(map.containsKey(s))
-            return map.get(s);
-     
-        List<String> list=new ArrayList<>();
-        if(0==s.length()){
+    private List<String> dfs(String s,List<String> wordDict){
+        if(map.containsKey(s))return map.get(s);
+        List<String> list=new ArrayList<String>();
+        if(s.length()==0){
             list.add("");
             return list;
         }
         for(String word:wordDict){
             if(s.startsWith(word)){
                 List<String> sub=dfs(s.substring(word.length()),wordDict);
-                for(String str:sub)
-                {
+                for(String str:sub){
                     if(str.length()>0)
-                        str=" "+str;
-                    list.add(word+str);
-                }
+                        list.add(word+" "+str);
+                    else
+                        list.add(word);
+                }   
             }
         }
         map.put(s,list);
