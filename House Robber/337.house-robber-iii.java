e9822fc6e9d26de16aff2ab07d611c748cec43d7
YenChen Liu <yenchen.liu@ubiik.com>
2021-07-06 17:18:56 +0800

    add 337

---
+++ House Robber/337.house-robber-iii.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=337 lang=java
+ *
+ * [337] House Robber III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res=dfs(root);
+        return Math.max(res[0],res[1]);
+    }
+    private int[] dfs(TreeNode root){
+
+        if(root==null)return new int[2];
+
+        int[] left=dfs(root.left);
+        int[] right=dfs(root.right);
+
+        int[] res=new int[2];
+        res[0] = root.val+left[1]+right[1];
+        res[1] = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        return res;
+    }
+}
+// @lc code=end
+
