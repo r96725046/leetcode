6a0a9728a24bc092405ba1e20b113bc47292894c
r96725046 <83388487+r96725046@users.noreply.github.com>
2021-09-09 09:05:27 +0800

    Update 1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.java

--- 1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -7,8 +7,12 @@
 // @lc code=start
 class Solution {
     public int minNumberOperations(int[] target) {
+        int count=target[0];
         
+        for(int i=1;i<target.length;i++){
+            count+=Math.max(target[i]-target[i-1],0);
+        }
+        return count;
     }
 }
 // @lc code=end
-
