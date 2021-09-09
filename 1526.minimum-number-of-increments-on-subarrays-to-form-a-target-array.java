5f4db56afd4ba0881dac23b8e12d64b78af9df9e
r96725046 <83388487+r96725046@users.noreply.github.com>
2021-09-09 09:03:40 +0800

    add 1526

---
+++ 1526.minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1526 lang=java
+ *
+ * [1526] Minimum Number of Increments on Subarrays to Form a Target Array
+ */
+
+// @lc code=start
+class Solution {
+    public int minNumberOperations(int[] target) {
+        
+    }
+}
+// @lc code=end
+
