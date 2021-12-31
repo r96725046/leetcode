4df4251120b8435d751b17b3e3bf14952fc16bdc
r96725046 <r96725046@ntu.edu.tw>
2021-12-31 12:26:37 +0800

    add 419,1367

---
+++ 1367.linked-list-in-binary-tree.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=1367 lang=java
+ *
+ * [1367] Linked List in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //1.dfs(head,root)|| isSubPath(head,root.left)||isSubPath(head,root.right);
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head==null)return true;
+        if(root==null)return false;
+        return dfs(head,root)|| isSubPath(head,root.left)||isSubPath(head,root.right);
+    }
+    private boolean dfs(ListNode head,TreeNode root){
+        if(head==null)return true;
+        if(root==null)return false;
+        
+        return head.val==root.val && (dfs(head.next,root.left)||dfs(head.next,root.right));
+    }
+}
+// @lc code=end
+
---
+++ 419.battleships-in-a-board.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=419 lang=java
+ *
+ * [419] Battleships in a Board
+ */
+
+// @lc code=start
+class Solution {
+    public int countBattleships(char[][] board) {
+        int count=0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='X'&& (i==0||board[i-1][j]=='.')&&(j==0||board[i][j-1]=='.'))count++;
+                
+            }
+        }
+        return count;
+    }
+}
+// @lc code=end
+
